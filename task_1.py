d4b44af23c6caf32edaa3adfb606e0c0b33b4550
Stanislav <stanislav.rossolenko@gmail.com>
2021-04-16 15:21:54 +0300

    Lesson_1

---
+++ task_1.py
@@ -0,0 +1,11 @@
+a = 10
+b = a + 1
+print(a + b)
+
+age = 15
+print(age)
+
+name = input('Введите ваше имя: ')
+answer = int(input('Введите ваш возраст: '))
+print('Ваше имя:', name ,'Ваш возраст: ', answer)
+
